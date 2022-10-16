a6cc646ea2f2d76bac3250004155134c789e10f7
unmukt <unmukt13@gmail.com>
2022-10-16 15:15:47 +0200

    read_until instead of readline

--- serial_test.py
+++ serial_test.py
@@ -5,11 +5,12 @@ import time
 with serial.Serial('COM3',9600) as ser:
     #ser.open()
     print("connected to port",ser.portstr,"\n")
-    print(ser.readlines().decode("utf"))
+    #print(ser.readlines().decode("utf"))
     while(True):
         if ser.in_waiting:
-            packet=ser.readlines()
-            print("read from arduino:")
+            #print("serial is available")
+            packet=ser.read_until()
+            #print("read from arduino:")
             print(packet.decode('utf')) #because arduino sends info in utf to serial, we want it to be understandable so wee decode
 
     
