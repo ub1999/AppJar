6abb0d4dcfbc7b485019f3bc1905fb7998d250e0
unmukt <unmukt13@gmail.com>
2022-10-20 21:13:46 +0200

    update

---
+++ tkinter_test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+
+# place a label on the root window
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+
+# keep the window displaying
+root.mainloop()
