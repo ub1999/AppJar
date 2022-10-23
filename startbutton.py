ae5d820bb1dd96e4fe2104f3eb1721767533a2f8
unmukt <unmukt13@gmail.com>
2022-10-23 19:06:34 +0200

    works now

--- startbutton.py
+++ startbutton.py
@@ -1,50 +1,7 @@
-#imports 
-from asyncore import read
-from cgitb import text
-import serial
-import serial.tools.list_ports as port_list
-import time
-import os
+import startbutton_functions
 import tkinter as tk
-from turtle import position, title, window_height, window_width
-import string
-import re
 
 #from asyncio.timeouts import timeout
-
-
-
-def get_portlist():
-    portlist=[]
-    for p in port_list.comports():
-        #print(p)
-        portlist.append({p.name,p.description})
-    #print("Current ports on device:")
-    return(portlist)    
-
-def set_port_callback(dropdownitem:tk.StringVar,window: tk.Tk,label_item:tk.Label): 
-    label_item.config(text='Port is now: '+dropdownitem.get())
-
-def start_button_callback(DropDownItem:tk.StringVar, test_label:tk.Label,message='start'):
-    #this line: uses a regex.search to look for a match for COM# from the chosen dropdown value, group() returns the matched string
-    port_no=re.search(r'COM\d',DropDownItem.get()).group()
-    with serial.Serial(port_no,115200,timeout=2) as ser:
-        print('connected to port',ser.portstr)
-        print("setup complete")
-        #check if something is in the serial input buffer
-        if ser.in_waiting==0:
-            recieved_packet=ser.read_until()
-            message=message+'\n'
-            ser.write(message.encode('utf-8'))
-            #print('sending a packet',recieved_packet)
-        recieved_packet=ser.read_until()
-        test_label.config(text=recieved_packet.decode('utf')) 
-        print(recieved_packet.decode('utf'))
-
-
-        
-    
-
 #setup window settings
 setup_window= tk.Tk()
 setup_window.title('Setup')
@@ -66,18 +23,20 @@ setup_window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 info=tk.Label(setup_window,text='Please choose a serial port:')
 info.pack()
 selected=tk.StringVar()
-options=get_portlist()
+options=startbutton_functions.get_portlist()
+
 #print(re.match(r'COM\d',str(options[0])))
 selected.set(' ')
 drop_down=tk.OptionMenu(setup_window,selected,*options)
 drop_down.pack()
+
 #button to set
 setup_label=tk.Label(setup_window,text="port is :")
 setup_label.pack()
-button = tk.Button(setup_window , text = "set port" , command = lambda: set_port_callback(selected,setup_window,setup_label)).pack() 
+button = tk.Button(setup_window , text = "set port" , command = lambda: startbutton_functions.set_port_callback(selected,setup_window,setup_label)).pack() 
 
 rec_label=tk.Label(setup_window,text='Sent:')
 rec_label.pack()
-start_button=tk.Button(setup_window,text='Start',command= lambda: start_button_callback(selected,rec_label)).pack()
+start_button=tk.Button(setup_window,text='Start',command= lambda: startbutton_functions.start_button_callback(selected,rec_label)).pack()
 
 setup_window.mainloop()
---
+++ startbutton_functions.py
@@ -0,0 +1,42 @@
+
+#imports
+from asyncore import read
+from cgitb import text
+import serial
+import serial.tools.list_ports as port_list
+import time
+import os
+import tkinter as tk
+from turtle import position, title, window_height, window_width
+import string
+import re
+
+def get_portlist():
+    portlist=[]
+    for p in port_list.comports():
+        #print(p)
+        portlist.append({p.name,p.description})
+    #print("Current ports on device:")
+    return(portlist)    
+
+def set_port_callback(dropdownitem:tk.StringVar,window: tk.Tk,label_item:tk.Label): 
+    label_item.config(text='Port is now: '+dropdownitem.get())
+
+def start_button_callback(DropDownItem:tk.StringVar, test_label:tk.Label,message='start'):
+    #this line: uses a regex.search to look for a match for COM# from the chosen dropdown value, group() returns the matched string
+    port_no=re.search(r'COM\d',DropDownItem.get()).group()
+    with serial.Serial(port_no,115200,timeout=2) as ser:
+        print('connected to port',ser.portstr)
+        print("setup complete")
+        #check if something is in the serial input buffer
+        if ser.in_waiting==0:
+            recieved_packet=ser.read_until()
+            message=message+'\n'
+            ser.write(message.encode('utf-8'))
+            #print('sending a packet',recieved_packet)
+        recieved_packet=ser.read_until()
+        test_label.config(text=recieved_packet.decode('utf')) 
+        print(recieved_packet.decode('utf'))
+
+
+        
