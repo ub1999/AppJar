b4fa81f4fe5ba7f5ede56612ca5f40c81b572e40
ub1999 <unmukt13@gmail.com>
2022-10-27 01:51:34 +0200

    changed the setuip function such that only 1 serial object is created for the gui, no need to reset connections the whole time

--- startbutton_functions.py
+++ startbutton_functions.py
@@ -12,7 +12,7 @@ import string
 import re
 from sys import platform
 
-
+ser1=serial.Serial()
 
 def get_portlist():
     portlist=[]
@@ -22,10 +22,8 @@ def get_portlist():
     #print("Current ports on device:")
     return(portlist)    
 
-def set_port_callback(dropdownitem:tk.StringVar,window: tk.Tk,label_item:tk.Label): 
-    label_item.config(text='Port is now: '+dropdownitem.get())
-
-def start_button_callback(DropDownItem:tk.StringVar, test_label:tk.Label,message='start'):
+def set_port_callback(DropDownItem:tk.StringVar,window: tk.Tk,label_item:tk.Label): 
+    ser1.baudrate=115200
     port_no=''
     
     #write a code for linux laptops:
@@ -38,22 +36,40 @@ def start_button_callback(DropDownItem:tk.StringVar, test_label:tk.Label,message
     elif platform == "win32":
     # Windows...
         port_no=re.search(r'COM\d',DropDownItem.get()).group()
-        
-    print(port_no)
-    
-    with serial.Serial(port_no,115200,timeout=2) as ser:
-        print('connected to port',ser.portstr)
-        print("setup complete")
-        #check if something is in the serial input buffer
-        if ser.in_waiting==0:
-            print('Sending:'+message)
-            recieved_packet=ser.read_until()
-            message=message+'\n'
-            ser.write(message.encode('utf-8'))
-            #print('sending a packet',recieved_packet)
-        recieved_packet=ser.read_until()
-        test_label.config(text=recieved_packet.decode('utf')) 
-        print(recieved_packet.decode('utf'))
+    ser1.port=port_no
+    ser1.timeout=2
+    ser1.close()
+    label_item.config(text='Port is now: '+port_no)
+    print('connected to port',ser1.portstr)
+    print("setup complete")
 
+def start_button_callback(DropDownItem:tk.StringVar, test_label:tk.Label,message='start'):
+    ser1.open()
+    #somehow everything fallsapart without this i dont understand why
+    recieved_packet=ser1.read_until()
+    
+    message=message+'\n'
+    ser1.write(message.encode('utf-8'))
+    print('Sending:'+message)
+    
+    #wait for out buffer to be empty
+    while ser1.out_waiting:
+        continue
+    
+    #print(r'write buffer empty')
+
+    while 1: 
+    #print(r'read buffer not empty',ser1.in_waiting)
+        if ser1.in_waiting:   
+            while ser1.in_waiting:
+                #print('in read buffer a packet')
+                recieved_packet=ser1.read_until().decode('utf')
+                recieved_packet=str(re.sub(r'[\n\r]',"",recieved_packet))
+                test_label.config(text=recieved_packet) 
+                print(recieved_packet)
+            break
+        else:
+            pass
+    ser1.close()
 
         
